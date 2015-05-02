95fd60ae495b69cf5a84a2dd0e964bb041eadcfe
Derrick Ho <derrickho328@gmail.com>
2015-05-01 19:52:33 -0700

    Initial Commit

---
+++ Giphy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Giphy
+//
+//  Created by Derrick  Ho on 5/1/15.
+//  Copyright (c) 2015 Derrick  Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
